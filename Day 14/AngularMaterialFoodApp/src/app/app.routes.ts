42b2b83e13cf7928ad21f3d2264725a1f48255dc
NaveenChandraBairaju <naveenraj1315@gmail.com>
2025-11-16 11:36:48 +0530

    Day 14 – Angular Materials

---
+++ Day 14/AngularMaterialFoodApp/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { FeedbackRatingComponent } from './feedback-rating/feedback-rating.component';
+
+export const routes: Routes = [
+  { path: '', component: FeedbackRatingComponent }
+];
